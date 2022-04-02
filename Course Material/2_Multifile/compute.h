b3c37537c0e327c8b956f342d7b5d9ab2f9609d0
Hrithik <hrithiksagar36@gmail.com>
2022-04-02 15:24:37 +0530

    Update

---
+++ Course Material/2_Multifile/compute.h
@@ -0,0 +1,9 @@
+#ifndef __COMPUTE_H__
+#define __COMPUTE_H__
+
+complex_t complex_sum(complex_t C1, complex_t C2);
+complex_t complex_difference(complex_t C1, complex_t C2);
+complex_t complex_multiply(complex_t C1, complex_t C2);
+complex_t complex_divide(complex_t C1, complex_t C2);
+
+#endif
